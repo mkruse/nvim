33e0b14d256953472a7cdc912772479e933acada
Marc Kruse <kruse@nwit.de>
2024-11-10 23:57:16 +0100

    Use lualine plugin for better statusline

--- lua/mkruse/lazy.lua
+++ lua/mkruse/lazy.lua
@@ -12,7 +12,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("mkruse.plugins", {
-  change_detection = {
+ checker = {
+    enabled = true,
+    notify = false,
+  },
+ change_detection = {
     notify = false,
   },
 })
