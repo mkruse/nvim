c9110bfa5baa287e510dff030c524ac8f3a89069
Marc Kruse <kruse@nwit.de>
2024-11-10 16:28:23 +0100

    Added vim-be-good plugin

--- lua/mkruse/plugins/init.lua
+++ lua/mkruse/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "ThePrimeagen/vim-be-good", -- little games to learn vim motions
 }
