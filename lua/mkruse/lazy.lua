b20bad5d9440275dc5b31b91e0a58a750f0f4248
Marc Kruse <kruse@nwit.de>
2024-11-10 23:45:37 +0100

    Disable lazy.vim plugin change detection

--- lua/mkruse/lazy.lua
+++ lua/mkruse/lazy.lua
@@ -11,4 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("mkruse.plugins")
+require("lazy").setup("mkruse.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
+
